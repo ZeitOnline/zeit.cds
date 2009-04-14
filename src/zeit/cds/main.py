fc310d7c68bf4a2b14060c1afc9f854ad6e9bd5d
Martijn Faassen <faassen@startifact.com>
2009-04-14 12:42:27 +0000

    Introduce logging for the success case.

--- src/zeit/cds/main.py
+++ src/zeit/cds/main.py
@@ -7,6 +7,7 @@ import ftputil
 import ftplib
 import gocept.filestore
 import os
+import logging
 
 
 class FTPSession(ftplib.FTP):
@@ -17,26 +18,41 @@ class FTPSession(ftplib.FTP):
         self.login(userid, password)
 
 def export(store_dir, server, port, user, password, upload_dir):
+    logging.info(
+        "Connecting to CDS FTP server "
+        "(host %s, port %s, user %s, upload_dir %s)" %
+        (server, port, user, upload_dir))                 
     host = ftputil.FTPHost(
         server, user, password, port=port, session_factory=FTPSession)
 
     filestore = gocept.filestore.filestore.FileStore(store_dir)
     filestore.prepare()
-
+    logging.info("Accessed filestore %s" % store_dir)
+    
     write_lock_path = os.path.join(upload_dir, 'write')
     if host.path.exists(write_lock_path):
-        #XXX: Logging
+        logging.info("Cannot write on CDS FTP server as 'write' lockfile "
+                     "exists. Exiting.")
         return
+
+    logging.info("Creating lock file")
     f = host.open(write_lock_path, mode='wb')
     f.write('This is a lock file.')
     f.close()
-
+    logging.info("Lock file successfully created")
+    
     try:
         for item in filestore.list('new'):
             item_name = os.path.basename(item)
             upload_path = os.path.join(upload_dir, item_name)
             host.upload(item, upload_path, mode='b')
+            logging.info("Uploaded file %s" % item_name)
             filestore.move(item_name, 'new', 'cur')
+            logging.info("Moved file %s from 'new' to 'cur'" % item_name)
     finally:
+        logging.info("Removing lock file")
         host.remove(write_lock_path)
+        logging.info("Lock file successfully removed")
         host.close()
+        logging.info("Disconnected from CDS FTP server")
+
